8b073a3a6040fc03fd97df3eeb19afdacd93889f
Lilly Daniell <daniell.lilly@gmail.com>
2021-11-03 11:29:30 +0000

    Add rails production ENV VAR to load db

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require_relative "application"
-
+RAILS_ENV=production bundle exec rake db:create db:schema:load
 # Initialize the Rails application.
 Rails.application.initialize!
