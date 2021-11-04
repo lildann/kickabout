c774722f08b8b49f11b7f6d7d9ddbf7a2ec2738e
Shakira <shakira.r.salazar@gmail.com>
2021-11-04 13:05:36 +0000

    save current user id as game id

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,6 +10,7 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
+		@game.user_id = current_user.id
     if @game.save
       redirect_to games_path
     else 
