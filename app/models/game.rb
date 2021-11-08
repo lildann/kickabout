3cd287004b0094f9f7d0aaae2066ae0ba923e76c
Lilly Daniell <daniell.lilly@gmail.com>
2021-11-08 10:40:38 +0000

    Begin edit / delete game functionality

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ApplicationRecord
+  validates :location, :player_count, :datetime, presence: true
   belongs_to :user, optional: true
   has_many :game_users
   has_many :users, :through => :game_users
