20ce7cd993ec10d58dedd1d6162cb8bfd8b2aeef
Shakira <shakira.r.salazar@gmail.com>
2021-11-11 17:30:51 +0000

    Fix everything!

--- app/javascript/controllers/landing_controller.js
+++ app/javascript/controllers/landing_controller.js
@@ -1,13 +1,6 @@
 import { Controller } from "stimulus"
 
-export function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+
 
 export default class extends Controller{
 
--- app/javascript/packs/kickAbout.js
+++ app/javascript/packs/kickAbout.js
@@ -1,23 +1,35 @@
-
-
-
-function openLogin(){
-	const loginForm = document.getElementById('login_form')
-	if(loginForm.style.display = "block"){
-		loginForm.style.display == "none"
-	}else{
-		loginForm.style.display == "block"
-	}
+export function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
+
 function openSignup(){
 	const signUpForm = document.getElementById('sign_up_form')
-	if(signUpForm.style.display == "none"){
-		signUpForm.style.display == "block"
+	const loginForm = document.getElementById('login_form')
+	if(signUpForm.style.display == "block"){
+		signUpForm.style.display = "none"
 	}else{
-		signUpForm.style.display == "none"
+		loginForm.style.display = "none"
+		signUpForm.style.display = "block"
+	}
+}
+function openLogin(){
+	const loginForm = document.getElementById('login_form')
+	const signUpForm = document.getElementById('sign_up_form')
+	if(loginForm.style.display == "block"){
+		loginForm.style.display = "none"
+	}else{
+		loginForm.style.display = "block"
+		signUpForm.style.display = "none"
 	}
 }
 
 window.openLogin = openLogin
 window.openSignup = openSignup
+
+
 
